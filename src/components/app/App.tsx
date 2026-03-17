fa59b3a2d1c522939a8759a2cb75d812d7cc7fb7
Senior <maxim.ostrovskiy86@gmail.com>
2026-03-17 10:17:21 +0000

    feat: added SearcBar MoviedGrids and Loaders components

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,13 +1,17 @@
-import './App.css'
+import "./App.module.css";
+import SearchBar from "../searchBar/SearchBar.tsx";
+import MovieGrid from "../movieGrid/MovieGrid.tsx";
+import Loader from "../loader/Loader.tsx";
 
 function App() {
 
   return (
     <>
-      <section id="center">
-      </section>
+      <SearchBar />
+      <MovieGrid />
+      <Loader/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
