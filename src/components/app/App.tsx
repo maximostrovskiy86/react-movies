d52a53b4163208bbe0dfd72a1ffa956914d5cd18
Senior <maxim.ostrovskiy86@gmail.com>
2026-03-18 13:30:49 +0000

    feat: added the trendingMovie Request + TS

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,16 +1,35 @@
-import "./App.module.css";
+import { useState, useEffect } from "react";
+import css from "./App.module.css";
 import SearchBar from "../searchBar/SearchBar.tsx";
 import MovieGrid from "../movieGrid/MovieGrid.tsx";
 import Loader from "../loader/Loader.tsx";
+import moviesApi from "../../services/servicesApi.ts";
+import type { Movie } from '../../types/movie.ts';
+
+
 
 function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    async function fetchMovies() {
+      try {
+        const data = await moviesApi.fetchGetMediaTrending();
+        setMovies(data);
+      } catch(error) {
+        console.error("Error loading movies:", error);
+      }
+    }
+
+    void fetchMovies();
+  }, []);
 
   return (
-    <>
+    <div className={css.container}>
       <SearchBar />
-      <MovieGrid />
-      <Loader/>
-    </>
+      <MovieGrid movies={movies}/>
+      <Loader />
+    </div>
   );
 }
 
